40e427bd3593e81d043d9db424367ef5d99fae8c
Anıl Altınkaya <anila@ernamas.com>
2023-03-07 17:29:34 +0300

    Popup pencere kodu eklendi

--- cvPageWeb/Models/Entity/Model1.Designer.cs
+++ cvPageWeb/Models/Entity/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\anil.altinkaya\Desktop\Anıl\CvUpp\cvpageweb\cvPageWeb\cvPageWeb\Models\Entity\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Anil\Source\Repos\cvPageWeb\cvPageWeb\Models\Entity\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
